ff6abaf9812a0a3c177395f021e4006479847dac
jiteshsh <35718282+jiteshsh@users.noreply.github.com>
2018-09-21 12:08:55 +0530

    Created docker_install.ps1 file

---
+++ docker_install.ps1
@@ -0,0 +1,25 @@
+function docker_install {
+
+    function check_installation
+        {        
+            $installed_package = Get-Package  
+            if ($installed_package.name -match 'docker' -and $installed_package.name -match 'DockerMsftProvider') {
+                return $true
+    
+            } else {Write-Host "Docker Module is not installed"
+                return $false
+            } 
+        }
+
+    if ((check_installation) -eq $true) {write-host "Docker is already installed" -ForegroundColor Cyan}
+    
+    if ((check_installation) -eq $false) {
+        Write-Host "Installing Docker !!!!" -ForegroundColor DarkCyan
+        Install-Module DockerMsftProvider -Force -ErrorAction Stop  | Out-Null
+        Install-Package Docker -ProviderName DockerMsftProvider -Force -ErrorAction Stop | Out-Null
+
+        if((check_installation) -eq $true) {echo "Installation was success"}
+    }
+}
+
+docker_install
