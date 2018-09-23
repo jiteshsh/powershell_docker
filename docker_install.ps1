109a900b5a0f79782eefc3d880c390fcd917b27e
jiteshsh <35718282+jiteshsh@users.noreply.github.com>
2018-09-23 21:44:20 +0530

    Added function to check the service status, if not started start it

--- docker_install.ps1
+++ docker_install.ps1
@@ -10,15 +10,35 @@ function docker_install {
                 return $false
             } 
         }
-
-    if ((check_installation) -eq $true) {write-host "Docker is already installed" -ForegroundColor Cyan}
+    
+    if ((check_installation) -eq $true) {write-host "Docker is already installed :) " -ForegroundColor Cyan}
     
     if ((check_installation) -eq $false) {
         Write-Host "Installing Docker !!!!" -ForegroundColor DarkCyan
         Install-Module DockerMsftProvider -Force -ErrorAction Stop  | Out-Null
         Install-Package Docker -ProviderName DockerMsftProvider -Force -ErrorAction Stop | Out-Null
 
-        if((check_installation) -eq $true) {echo "Installation Completed :) "}
+        if((check_installation) -eq $true) {echo "Installation was success"}
+    }
+    
+    function check_service {    
+        $docker_status = (get-service Docker).Status 
+                            
+        if ($docker_status -eq 'Stopped') {               
+            return $false 
+        }
+        elseif ($docker_status -eq 'Running') {             
+            return $true 
+        }
+        else {echo "Somehting went wrong with docker service"
+            break             
+        }
+    }      
+    
+    if ((check_service) -eq $false) {
+        Write-Host "Currently Docker engine not running" -ForegroundColor Cyan        
+        Start-Service docker -Verbose
+        if ((check_service) -eq $true) {Write-Host "Docker engine is running now" -ForegroundColor cyan}
     }
 }
 
